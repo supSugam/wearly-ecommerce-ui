789b9a7c8c63992d725c4e7963141d2cff594bab
supSugam <sugamsubedi234@gmail.com>
2023-04-06 16:43:07 +0545

    Built Featured Section and Product Cards

---
+++ js/effects.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const featuredBox = document.querySelectorAll(".featured-box");
+
+featuredBox.forEach(function (box) {
+	box.addEventListener("mouseenter", function (e) {
+		const textBox = e.target.querySelector(".featured-box__text");
+		const textEl = textBox.querySelectorAll("h1,h3");
+
+		const gradientEl = textBox.querySelector(".gradient-text");
+
+		gradientEl.classList.remove("gradient-text");
+		gradientEl.classList.add("text--blue");
+
+		textEl.forEach((text) => {
+			text.classList.add("text--active");
+		});
+	});
+
+	box.addEventListener("mouseleave", function (e) {
+		const textBox = e.target.querySelector(".featured-box__text");
+		const textEl = textBox.querySelectorAll("h1,h3");
+
+		const gradientEl = textBox.querySelector(".text--blue");
+		gradientEl.classList.add("gradient-text");
+		gradientEl.classList.remove("text--blue");
+
+		textEl.forEach((text) => {
+			text.classList.remove("text--active");
+		});
+	});
+});
