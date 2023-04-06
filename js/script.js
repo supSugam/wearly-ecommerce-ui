3c54d8f58d92bba694ec4411bf40b6e66cd4a5bf
supSugam <sugamsubedi234@gmail.com>
2023-04-06 22:36:13 +0545

    Product Cards - Almost completed

---
+++ js/script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const quantityInputs = document.querySelectorAll(".quantity-input");
+
+quantityInputs.forEach((input) => {
+	const minusBtn = input.querySelector(".minus");
+	const plusBtn = input.querySelector(".plus");
+	const quantityValue = input.querySelector(".quantity-value");
+
+	minusBtn.addEventListener("click", () => {
+		if (quantityValue.value > parseInt(quantityValue.min)) {
+			quantityValue.value = parseInt(quantityValue.value) - 1;
+		}
+	});
+
+	plusBtn.addEventListener("click", () => {
+		if (quantityValue.value < parseInt(quantityValue.max)) {
+			quantityValue.value = parseInt(quantityValue.value) + 1;
+		}
+	});
+});
+
+// Ratings Calculator
+
+function displayStarRating(value) {
+	const fullStar = '<i class="fa-solid fa-star"></i>';
+	const halfStar = '<i class="fa-solid fa-star-half-stroke"></i>';
+	const emptyStar = '<i class="fa-regular fa-star"></i>';
+
+	const roundedValue = Math.round(value * 2) / 2; // round to nearest 0.5
+
+	let starsHtml = "";
+	for (let i = 0; i < Math.floor(roundedValue); i++) {
+		starsHtml += fullStar;
+	}
+	if (roundedValue % 1 >= 0.5) {
+		starsHtml += halfStar;
+	}
+	for (let i = Math.ceil(roundedValue); i < 5; i++) {
+		starsHtml += emptyStar;
+	}
+
+	return starsHtml;
+}
+
+// Example usage:
+const ratingContainer = document.querySelector(".product-rating");
+ratingContainer.innerHTML = displayStarRating(3.3);
