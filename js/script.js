741251efe5f3b4ed8976b7e0b0a8de842b837781
supSugam <sugamsubedi234@gmail.com>
2023-04-08 06:35:20 +0545

    Made Login Page and Sign up soon

--- js/script.js
+++ js/script.js
@@ -46,3 +46,24 @@ function displayStarRating(value) {
 // Example usage:
 const ratingContainer = document.querySelector(".product-rating");
 ratingContainer.innerHTML = displayStarRating(3.3);
+
+// Spinner
+
+// Get the button element
+const btnCart = document.querySelector(".btn--cart");
+
+// Add a click event listener to the button
+btnCart.addEventListener("click", function () {
+	// Replace the original icon with a loading icon
+	btnCart.innerHTML =
+		'Add to Cart <i class="fa-duotone fa-spinner-third fa-spin"></i>';
+
+	// Wait for one second (1000 milliseconds)
+	setTimeout(function () {
+		// Redirect to the other page
+		window.location.href = "https://supSugam.github.io/wearly-ecommerce-ui";
+
+		// Replace the loading icon with the original icon
+		btnCart.innerHTML = '<i class="ph-fill ph-shopping-cart"></i> Add to cart';
+	}, 1500);
+});
