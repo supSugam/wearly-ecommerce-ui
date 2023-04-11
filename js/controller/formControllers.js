ab8e7b9afaf32f566ef3e9ea773c2914d8314e9f
supSugam <sugamsubedi234@gmail.com>
2023-04-11 21:50:38 +0545

    Built add product page for admin

--- js/controller/formControllers.js
+++ js/controller/formControllers.js
@@ -11,7 +11,7 @@ const validateSignupForm = function () {
 		"input[type=checkbox][required]"
 	);
 
-	const checkBoxText = document
+	const checkBoxText = signupForm
 		.querySelector(".label-checkmark")
 		.querySelector("p");
 
@@ -105,3 +105,121 @@ const validateLoginForm = function () {
 	// submit form if validation is successful
 	return true;
 };
+
+const validateAddProductForm = function () {
+	const addProductForm = document.querySelector(".add-product-form");
+	const inputFields = addProductForm.querySelectorAll(
+		"input[type=text][required], input[type=number][required], textarea[required]"
+	);
+	const imageInput = addProductForm.querySelector("input[type=file][required]");
+
+	// const checkCheckBox = function () {
+	// 	if (!checkBoxInput.checked) {
+	// 		isValid = false;
+	// 		checkBoxText.classList.add("error-checkbox");
+	// 	} else {
+	// 		checkBoxText.classList.remove("error-checkbox");
+	// 	}
+	// };
+	// checkCheckBox();
+
+	// checkBoxInput.addEventListener("change", () => {
+	// 	checkCheckBox();
+	// });
+
+	// const checkBoxText = signupForm
+	// 	.querySelector(".label-checkmark")
+	// 	.querySelector("p");
+
+	let isValid = true;
+
+	inputFields.forEach((inputField) => {
+		inputField.addEventListener("input", () => {
+			inputField.classList.remove("error");
+			inputField
+				.closest(".input-field-container")
+				.querySelector(".error-message")
+				.classList.remove("show");
+		});
+
+		if (!inputField.value) {
+			isValid = false;
+			inputField.classList.add("error");
+			inputField
+				.closest(".input-field-container")
+				.querySelector(".error-message")
+				.classList.add("show");
+		}
+	});
+
+	if (imageInput.files.length !== 1) {
+		isValid = false;
+		addProductForm
+			.querySelector(".product-preview")
+			.classList.add("error-image");
+	}
+
+	if (!isValid) return false;
+
+	document.querySelector(".btn-signup").innerHTML =
+		'Signing Up.. <i class="fa-duotone fa-spinner-third fa-spin"></i>';
+
+	// submit form if validation is successful
+	return true;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+	const forms = document.querySelectorAll("form");
+	forms.forEach((form) => {
+		form.addEventListener("submit", (e) => {
+			e.preventDefault();
+		});
+	});
+
+	const checkBoxGender = document
+		.querySelector(".add-product-form")
+		.querySelector(".category-gender")
+		.querySelectorAll("input[type=checkbox]");
+
+	checkBoxGender.forEach((checkbox) => {
+		checkbox.addEventListener("click", (event) => {
+			// If the clicked checkbox is now checked
+			if (event.target.checked) {
+				// Uncheck the other checkboxes
+				checkBoxGender.forEach((otherCheckbox) => {
+					if (otherCheckbox !== event.target) {
+						otherCheckbox.checked = false;
+					}
+				});
+			}
+		});
+	});
+
+	const checkBoxCategory = document
+		.querySelector(".add-product-form")
+		.querySelector(".category-category")
+		.querySelectorAll("input[type=checkbox]");
+
+	let checkedCategory = [];
+
+	let lastCheckedCheckbox = null;
+
+	checkBoxCategory.forEach((checkbox) => {
+		checkbox.addEventListener("click", (e) => {
+			if (e.target.checked) {
+				if (lastCheckedCheckbox !== null && lastCheckedCheckbox !== e.target) {
+					lastCheckedCheckbox.checked = false;
+					checkedCategory = [e.target.value];
+				} else {
+					checkedCategory.push(e.target.value);
+				}
+				lastCheckedCheckbox = e.target;
+			} else {
+				checkedCategory = checkedCategory.filter(
+					(value) => value !== e.target.value
+				);
+				lastCheckedCheckbox = null;
+			}
+		});
+	});
+});
---
+++ js/view/numberIncrement.js
@@ -0,0 +1,22 @@
+const inputField = document.getElementById("product-quantity");
+const ispinUp = document.getElementById("ispin-up");
+const ispinDown = document.getElementById("ispin-down");
+
+// Initialize the value to 0
+// let value = 0;
+
+// Increment the value and update the input field
+ispinUp.addEventListener("click", () => {
+	let value = parseInt(inputField.value);
+	if (!value) value = 0;
+	value += 5;
+	inputField.value = value;
+});
+
+// Decrement the value and update the input field
+ispinDown.addEventListener("click", () => {
+	if (value > 0) {
+		value -= 5;
+		inputField.value = value;
+	}
+});
