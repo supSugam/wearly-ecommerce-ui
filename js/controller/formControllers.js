aab556d4cf65341257c0e88729a143fa1ddebb9c
supSugam <sugamsubedi234@gmail.com>
2023-04-12 11:52:39 +0545

    SIgnup form validation added

--- js/controller/formControllers.js
+++ js/controller/formControllers.js
@@ -3,13 +3,36 @@
 // Method to validate the form
 const validateSignupForm = function () {
 	const signupForm = document.querySelector(".signup-form");
+
 	const inputFields = signupForm.querySelectorAll(
-		"input[type=text][required], input[type=email][required], input[type=password][required], input[type=tel][required]"
+		"input[type=text][required], input[type=email][required], input[type=password][required], input[type=number][required]"
 	);
 	const imageInput = signupForm.querySelector("input[type=file][required]");
 	const checkBoxInput = signupForm.querySelector(
 		"input[type=checkbox][required]"
 	);
+
+	const showError = function (
+		inputField,
+		message = "You must fill this field."
+	) {
+		inputField.classList.add("error");
+		inputField
+			.closest(".input-field-container")
+			.querySelector(".error-message")
+			.classList.add("show");
+		inputField
+			.closest(".input-field-container")
+			.querySelector(".error-message").innerHTML = message;
+	};
+
+	const removeError = function (inputField) {
+		inputField.classList.remove("error");
+		inputField
+			.closest(".input-field-container")
+			.querySelector(".error-message")
+			.classList.remove("show");
+	};
 
 	const checkBoxText = signupForm
 		.querySelector(".label-checkmark")
@@ -18,22 +41,42 @@ const validateSignupForm = function () {
 	let isValid = true;
 
 	inputFields.forEach((inputField) => {
-		inputField.addEventListener("input", () => {
-			inputField.classList.remove("error");
-			inputField
-				.closest(".input-field-container")
-				.querySelector(".error-message")
-				.classList.remove("show");
-		});
-
-		if (!inputField.value) {
+		if (
+			!inputField.value &&
+			(inputField.type === "text" ||
+				inputField.type === "password" ||
+				inputField.type === "number" ||
+				inputField.type === "email")
+		) {
 			isValid = false;
-			inputField.classList.add("error");
-			inputField
-				.closest(".input-field-container")
-				.querySelector(".error-message")
-				.classList.add("show");
+			showError(inputField);
+		} else {
+			if (inputField.type === "email") {
+				const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+				if (!emailRegex.test(inputField.value)) {
+					isValid = false;
+					showError(inputField, "Enter a valid email address!");
+				}
+			}
+
+			if (inputField.type === "password") {
+				if (inputField.value.length < 8) {
+					isValid = false;
+					showError(inputField, "Must be at least 8 characters long!");
+				}
+			}
+
+			if (inputField.type === "number") {
+				if (inputField.value.length !== 10) {
+					isValid = false;
+					showError(inputField, "Must be 10 digits long!");
+				}
+			}
 		}
+
+		inputField.addEventListener("input", () => {
+			removeError(inputField);
+		});
 	});
 
 	if (imageInput.files.length !== 1) {
@@ -60,6 +103,10 @@ const validateSignupForm = function () {
 		'Signing Up.. <i class="fa-duotone fa-spinner-third fa-spin"></i>';
 
 	// submit form if validation is successful
+	const formData = new FormData(signupForm);
+	for (const [key, value] of formData) {
+		console.log(key, value);
+	}
 	return true;
 };
 
@@ -176,50 +223,50 @@ document.addEventListener("DOMContentLoaded", () => {
 		});
 	});
 
-	const checkBoxGender = document
-		.querySelector(".add-product-form")
-		.querySelector(".category-gender")
-		.querySelectorAll("input[type=checkbox]");
+	// const checkBoxGender = document
+	// 	.querySelector(".add-product-form")
+	// 	.querySelector(".category-gender")
+	// 	.querySelectorAll("input[type=checkbox]");
 
-	checkBoxGender.forEach((checkbox) => {
-		checkbox.addEventListener("click", (event) => {
-			// If the clicked checkbox is now checked
-			if (event.target.checked) {
-				// Uncheck the other checkboxes
-				checkBoxGender.forEach((otherCheckbox) => {
-					if (otherCheckbox !== event.target) {
-						otherCheckbox.checked = false;
-					}
-				});
-			}
-		});
-	});
+	// checkBoxGender.forEach((checkbox) => {
+	// 	checkbox.addEventListener("click", (event) => {
+	// 		// If the clicked checkbox is now checked
+	// 		if (event.target.checked) {
+	// 			// Uncheck the other checkboxes
+	// 			checkBoxGender.forEach((otherCheckbox) => {
+	// 				if (otherCheckbox !== event.target) {
+	// 					otherCheckbox.checked = false;
+	// 				}
+	// 			});
+	// 		}
+	// 	});
+	// });
 
-	const checkBoxCategory = document
-		.querySelector(".add-product-form")
-		.querySelector(".category-category")
-		.querySelectorAll("input[type=checkbox]");
+	// const checkBoxCategory = document
+	// 	.querySelector(".add-product-form")
+	// 	.querySelector(".category-category")
+	// 	.querySelectorAll("input[type=checkbox]");
 
-	let checkedCategory = [];
+	// let checkedCategory = [];
 
-	let lastCheckedCheckbox = null;
+	// let lastCheckedCheckbox = null;
 
-	checkBoxCategory.forEach((checkbox) => {
-		checkbox.addEventListener("click", (e) => {
-			if (e.target.checked) {
-				if (lastCheckedCheckbox !== null && lastCheckedCheckbox !== e.target) {
-					lastCheckedCheckbox.checked = false;
-					checkedCategory = [e.target.value];
-				} else {
-					checkedCategory.push(e.target.value);
-				}
-				lastCheckedCheckbox = e.target;
-			} else {
-				checkedCategory = checkedCategory.filter(
-					(value) => value !== e.target.value
-				);
-				lastCheckedCheckbox = null;
-			}
-		});
-	});
+	// checkBoxCategory.forEach((checkbox) => {
+	// 	checkbox.addEventListener("click", (e) => {
+	// 		if (e.target.checked) {
+	// 			if (lastCheckedCheckbox !== null && lastCheckedCheckbox !== e.target) {
+	// 				lastCheckedCheckbox.checked = false;
+	// 				checkedCategory = [e.target.value];
+	// 			} else {
+	// 				checkedCategory.push(e.target.value);
+	// 			}
+	// 			lastCheckedCheckbox = e.target;
+	// 		} else {
+	// 			checkedCategory = checkedCategory.filter(
+	// 				(value) => value !== e.target.value
+	// 			);
+	// 			lastCheckedCheckbox = null;
+	// 		}
+	// 	});
+	// });
 });
