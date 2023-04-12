994b87e1e9b88546250dcdc13fbefb7a40a1e6b7
supSugam <sugamsubedi234@gmail.com>
2023-04-12 22:40:48 +0545

    Added Dropdown to navbar

--- js/controller/formControllers.js
+++ js/controller/formControllers.js
@@ -1,5 +1,30 @@
 "use strict";
 
+const showError = function (inputField, message = "You must fill this field.") {
+	inputField.classList.add("error");
+	inputField
+		.closest(".input-field-container")
+		.querySelector(".error-message")
+		.classList.add("show");
+	inputField
+		.closest(".input-field-container")
+		.querySelector(".error-message").innerHTML = message;
+};
+
+const removeError = function (inputField) {
+	inputField.classList.remove("error");
+	inputField
+		.closest(".input-field-container")
+		.querySelector(".error-message")
+		.classList.remove("show");
+};
+
+// const validateCheckBox = function (checkBoxInput, checkBoxText) {
+// 	const isChecked = checkBoxInput.checked;
+// 	checkBoxText.classList.toggle("error-checkbox", !isChecked);
+// 	return isChecked;
+// };
+
 // Method to validate the form
 const validateSignupForm = function () {
 	const signupForm = document.querySelector(".signup-form");
@@ -8,48 +33,31 @@ const validateSignupForm = function () {
 		"input[type=text][required], input[type=email][required], input[type=password][required], input[type=number][required]"
 	);
 	const imageInput = signupForm.querySelector("input[type=file][required]");
-	const checkBoxInput = signupForm.querySelector(
-		"input[type=checkbox][required]"
-	);
 
-	const showError = function (
-		inputField,
-		message = "You must fill this field."
-	) {
-		inputField.classList.add("error");
-		inputField
-			.closest(".input-field-container")
-			.querySelector(".error-message")
-			.classList.add("show");
-		inputField
-			.closest(".input-field-container")
-			.querySelector(".error-message").innerHTML = message;
-	};
-
-	const removeError = function (inputField) {
-		inputField.classList.remove("error");
-		inputField
-			.closest(".input-field-container")
-			.querySelector(".error-message")
-			.classList.remove("show");
-	};
-
+	const checkBoxInput = signupForm.querySelector('input[type="checkbox"]');
 	const checkBoxText = signupForm
 		.querySelector(".label-checkmark")
 		.querySelector("p");
 
 	let isValid = true;
 
-	inputFields.forEach((inputField) => {
-		if (
-			!inputField.value &&
-			(inputField.type === "text" ||
-				inputField.type === "password" ||
-				inputField.type === "number" ||
-				inputField.type === "email")
-		) {
+	const validateCheckBox = function () {
+		if (checkBoxInput.checked) {
+			isValid = true;
+			checkBoxText.classList.remove("error-checkbox", isValid);
+		} else {
 			isValid = false;
-			showError(inputField);
+			checkBoxText.classList.add("error-checkbox", isValid);
+		}
+	};
+	validateCheckBox();
+
+	checkBoxInput.addEventListener("change", validateCheckBox);
+
+	inputFields.forEach((inputField) => {
+		if (!inputField.value) {
+			isValid = false;
+			showError(inputField, `${inputField.placeholder} is required.`);
 		} else {
 			if (inputField.type === "email") {
 				const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
@@ -84,19 +92,6 @@ const validateSignupForm = function () {
 		document.querySelector(".avatar-preview").classList.add("error-image");
 	}
 
-	const checkCheckBox = function () {
-		if (!checkBoxInput.checked) {
-			isValid = false;
-			checkBoxText.classList.add("error-checkbox");
-		} else {
-			checkBoxText.classList.remove("error-checkbox");
-		}
-	};
-	checkCheckBox();
-	checkBoxInput.addEventListener("change", () => {
-		checkCheckBox();
-	});
-
 	if (!isValid) return false;
 
 	document.querySelector(".btn-signup").innerHTML =
@@ -104,9 +99,17 @@ const validateSignupForm = function () {
 
 	// submit form if validation is successful
 	const formData = new FormData(signupForm);
-	for (const [key, value] of formData) {
-		console.log(key, value);
+	// create an object to store the form data
+	const formObject = {};
+
+	// iterate over the form data entries and add them to the object
+	for (const [key, value] of formData.entries()) {
+		key === "check" ? (formObject[key] = true) : (formObject[key] = value);
 	}
+
+	// convert the object to JSON using JSON.stringify()
+	const jsonData = JSON.stringify(formObject);
+	console.log(jsonData);
 	return true;
 };
 
@@ -153,68 +156,6 @@ const validateLoginForm = function () {
 	return true;
 };
 
-const validateAddProductForm = function () {
-	const addProductForm = document.querySelector(".add-product-form");
-	const inputFields = addProductForm.querySelectorAll(
-		"input[type=text][required], input[type=number][required], textarea[required]"
-	);
-	const imageInput = addProductForm.querySelector("input[type=file][required]");
-
-	// const checkCheckBox = function () {
-	// 	if (!checkBoxInput.checked) {
-	// 		isValid = false;
-	// 		checkBoxText.classList.add("error-checkbox");
-	// 	} else {
-	// 		checkBoxText.classList.remove("error-checkbox");
-	// 	}
-	// };
-	// checkCheckBox();
-
-	// checkBoxInput.addEventListener("change", () => {
-	// 	checkCheckBox();
-	// });
-
-	// const checkBoxText = signupForm
-	// 	.querySelector(".label-checkmark")
-	// 	.querySelector("p");
-
-	let isValid = true;
-
-	inputFields.forEach((inputField) => {
-		inputField.addEventListener("input", () => {
-			inputField.classList.remove("error");
-			inputField
-				.closest(".input-field-container")
-				.querySelector(".error-message")
-				.classList.remove("show");
-		});
-
-		if (!inputField.value) {
-			isValid = false;
-			inputField.classList.add("error");
-			inputField
-				.closest(".input-field-container")
-				.querySelector(".error-message")
-				.classList.add("show");
-		}
-	});
-
-	if (imageInput.files.length !== 1) {
-		isValid = false;
-		addProductForm
-			.querySelector(".product-preview")
-			.classList.add("error-image");
-	}
-
-	if (!isValid) return false;
-
-	document.querySelector(".btn-signup").innerHTML =
-		'Signing Up.. <i class="fa-duotone fa-spinner-third fa-spin"></i>';
-
-	// submit form if validation is successful
-	return true;
-};
-
 document.addEventListener("DOMContentLoaded", () => {
 	const forms = document.querySelectorAll("form");
 	forms.forEach((form) => {
@@ -222,6 +163,70 @@ document.addEventListener("DOMContentLoaded", () => {
 			e.preventDefault();
 		});
 	});
+
+	const validateAddProductForm = function () {
+		const addProductForm = document.querySelector(".add-product-form");
+		const inputFields = addProductForm.querySelectorAll(
+			"input[type=text][required], input[type=number][required], textarea[required]"
+		);
+		const imageInput = addProductForm.querySelector(
+			"input[type=file][required]"
+		);
+
+		// const checkCheckBox = function () {
+		// 	if (!checkBoxInput.checked) {
+		// 		isValid = false;
+		// 		checkBoxText.classList.add("error-checkbox");
+		// 	} else {
+		// 		checkBoxText.classList.remove("error-checkbox");
+		// 	}
+		// };
+		// checkCheckBox();
+
+		// checkBoxInput.addEventListener("change", () => {
+		// 	checkCheckBox();
+		// });
+
+		// const checkBoxText = signupForm
+		// 	.querySelector(".label-checkmark")
+		// 	.querySelector("p");
+
+		let isValid = true;
+
+		inputFields.forEach((inputField) => {
+			inputField.addEventListener("input", () => {
+				inputField.classList.remove("error");
+				inputField
+					.closest(".input-field-container")
+					.querySelector(".error-message")
+					.classList.remove("show");
+			});
+
+			if (!inputField.value) {
+				isValid = false;
+				inputField.classList.add("error");
+				inputField
+					.closest(".input-field-container")
+					.querySelector(".error-message")
+					.classList.add("show");
+			}
+		});
+
+		if (imageInput.files.length !== 1) {
+			isValid = false;
+			addProductForm
+				.querySelector(".product-preview")
+				.classList.add("error-image");
+		}
+
+		if (!isValid) return false;
+
+		document.querySelector(".btn-signup").innerHTML =
+			'Signing Up.. <i class="fa-duotone fa-spinner-third fa-spin"></i>';
+
+		// submit form if validation is successful
+		return true;
+	};
 
 	// const checkBoxGender = document
 	// 	.querySelector(".add-product-form")
